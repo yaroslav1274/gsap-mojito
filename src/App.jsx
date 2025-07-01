caea3125623ce4967ff8527aab7c8fd092331e3f
Yaroslav Kelemen <yaroslavkelemen14@gmail.com>
2025-07-01 19:45:26 +0300

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl">1e1e</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   );
 };
 
