9ece602188ba0cd47af1e979cb97d7e8176c0138
Yaroslav Kelemen <yaroslavkelemen14@gmail.com>
2025-07-02 18:45:35 +0300

    Implement Hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,38 @@
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  useGSAP(() => {
-    const heroSplit = new SplitText(".title", { type: "chars, words" });
-    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+  const videoRef = useRef();
 
-    heroSplit.chars.forEach((char, index) =>
-      char.classList.add("text-gradient")
-    );
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
+
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    // Apply text-gradient class once before animating
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 1.8,
-      ease: "expo.Out",
-      stagger: 0.05,
+      ease: "expo.out",
+      stagger: 0.06,
     });
 
     gsap.from(paragraphSplit.lines, {
       opacity: 0,
       yPercent: 100,
       duration: 1.8,
-      ease: "expo.Out",
+      ease: "expo.out",
       stagger: 0.06,
       delay: 1,
     });
@@ -36,20 +46,28 @@ const Hero = () => {
           scrub: true,
         },
       })
-      .to(
-        ".right-leaf",
-        {
-          y: 200,
-        },
-        0
-      )
-      .to(
-        ".left-leaf",
-        {
-          y: -200,
-        },
-        0
-      );
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -69,11 +87,12 @@ const Hero = () => {
         />
 
         <div className="body">
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
           <div className="content">
             <div className="space-y-5 hidden md:block">
               <p>Cool. Crisp. Classic.</p>
               <p className="subtitle">
-                {" "}
                 Sip the Spirit <br /> of Summer
               </p>
             </div>
@@ -84,11 +103,21 @@ const Hero = () => {
                 creative flair, and timeless recipes — designed to delight your
                 senses.
               </p>
-              <a href="#cocktails">View Cocktails</a>
+              <a href="#cocktails">View cocktails</a>
             </div>
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/src/public/videos/output.mp4"
+        />
+      </div>
     </>
   );
 };
